913b84ae6131e8d86ccc94903893bb4fb9fead85
rachid <rachid_oth@yahoo.fr>
2023-06-11 17:34:09 +0100

    Initialize the projet

---
+++ src/test/java/chaptre1/FirstClassTAU.java
@@ -0,0 +1,13 @@
+package chaptre1;
+
+import org.junit.Test;
+
+import static java.lang.System.*;
+
+public class FirstClassTAU {
+
+    @Test
+    public void firstAut(){
+        out.println("My First Test");
+    }
+}
